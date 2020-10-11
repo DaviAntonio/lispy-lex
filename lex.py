6808272aa10581dbabc5fc4ad45cff0ee7e9be8e
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2020-10-11 17:01:54 -0300

    Set up basic lexer and .gitignore
    Fix failures in the test by adding regular expressions for basic
    categories and removing the placeholder return value. Tokens identified
    as comments and whitespace will not be returned.
    
    Add some standalone test examples.
    
    Add .gitignore for Vim swap files and Python cache directory.

--- lex.py
+++ lex.py
@@ -12,5 +12,45 @@ def lex(code: str) -> Iterable[Token]:
     Retorna sequência de objetos do tipo token correspondendo à análise léxica
     da string de código fornecida.
     """
+    lex_rules = (
+            ("QUOTE", r"'"),
+            ("LPAR", r"\("),
+            ("RPAR", r"\)"),
+            ("COMMENT", r";+[^\n]+$"),
+            ("NUMBER", r"[+-]?[\d]+(?:\.[\d]+)?"),
+            # accepts letters, symbols and digits, but the first char cannot be a digit
+            ("NAME", r"[a-zA-Z+\-.*\/<=>!?:$%_&~^][\w+\-.*\/<=>!?:$%&~^]*"),
+            ("STRING", r"\".*\""),
+            # A "#\" followed by any printable characters
+            ("CHAR", r"#\\[!-~]*"),
+            ("BOOL", r"#[tf]"),
+            ("WHITESPACE", r"[\s]+"),
+            ("MISMATCH", r".")
+    )
 
-    return [Token('INVALIDA', 'valor inválido')]
+    tok_regex = '|'.join(f"(?P<{name}>{expr})" for name, expr in lex_rules)
+
+    tokens = []
+    for found in re.finditer(tok_regex, code):
+        group = found.lastgroup
+        content = found.group()
+
+        # Discard all whitespace and comments
+        if group != "WHITESPACE" and group != "COMMENT":
+            tokens.append(Token(group, content))
+
+    return tokens
+
+if __name__ == "__main__":
+    examples = (
+            "( a )",
+            "x ;; comentário",
+            '"The word \"recursion\" has many meanings."',
+            "'(+ 1 2)"
+    )
+
+    for ex in examples:
+        print(ex)
+        for tok in lex(ex):
+            print(tok)
+        print()
