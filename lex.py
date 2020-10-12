7693c9737191fd852863af39924c9d818e84bed6
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2020-10-12 00:03:18 -0300

    Accept integers, fractions and floating point
    The expression used to match numbers has been modified to match
    integers, fractions and floating points with exponents.
    
    Further standalone tests have been added to check the lexer
    identification of the new number types included.

--- lex.py
+++ lex.py
@@ -18,7 +18,8 @@ def lex(code: str) -> Iterable[Token]:
             ("RPAR", r"\)"),
             # ";" and all characters until a newline
             ("COMMENT", r";[^\n]*"),
-            ("NUMBER", r"[+-]?[\d]+(?:\.[\d]+)?"),
+            # accepts integers, fractions and floating point with exponents
+            ("NUMBER", r"[+-]?[\d]+(?:(?:\/[\d]+)|(?:(?:\.[\d]+)?(?:[esfdl][+-]?[\d]+)?))"),
             # accepts letters, symbols and digits, but the first char cannot be a digit
             ("NAME", r"[a-zA-Z+\-.*\/<=>!?:$%_&~^][\w+\-.*\/<=>!?:$%&~^]*"),
             ("STRING", r"\".*\""),
@@ -54,7 +55,34 @@ if __name__ == "__main__":
               (lambda (n)
                 (if (= n 0)
                     1        ;Base case: return 1
-                    (* n (fact (- n 1))))))"""
+                    (* n (fact (- n 1))))))""",
+            "0.0",
+            "0",
+            "1.5707963267948965",
+            "0.7615941559557649",
+            "1024",
+            "9/10",
+            "4/5",
+            "2.762e8",
+            "2.762e10",
+            "2.762e+10",
+            "2.762e-10",
+            "2.762s8",
+            "2.762s10",
+            "2.762s+10",
+            "2.762s-10",
+            "2.762f8",
+            "2.762f10",
+            "2.762f+10",
+            "2.762f-10",
+            "2.762d8",
+            "2.762f10",
+            "2.762f+10",
+            "2.762f-10",
+            "2.762l8",
+            "2.762l10",
+            "2.762l+10",
+            "2.762l-10"
     )
 
     for ex in examples:
